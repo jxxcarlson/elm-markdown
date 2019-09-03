0976adc1386e18c1b9a1bc551c7f76ecd27ebf24
Folkert de Vries <folkert@folkertdev.nl>
2019-09-03 20:56:35 +0200

    make LineTypeTests run

--- tests/LineTypeTests.elm
+++ tests/LineTypeTests.elm
@@ -1,64 +1,49 @@
-module LineTypeTests exposing (..)
+module LineTypeTests exposing (lineTypeTests)
 
+import BlockType exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Option
+import Parser.Advanced exposing (run)
 import Test exposing (..)
-import BlockType exposing(..)
-import Parser.Advanced exposing(run)
 
-suite : Test
-suite =
-    describe "The MMParser module"
-        [ lineTypeTests
 
-        ]
-
+parse =
+    BlockType.parse Option.ExtendedMath
 
 
 lineTypeTests =
     describe "BlockType.parse"
         [ test "code block" <|
-                \_ ->
-                 "```foo" |> run parse |>  Expect.equal (Ok (BalancedBlock DisplayCode))
-
-          , test "verbatim block" <|
-                \_ ->
-                 "````foo" |> run parse |>  Expect.equal (Ok (BalancedBlock Verbatim))
-
-          , test "display math block" <|
-                  \_ ->
-                       "$$\n" |> run parse |>  Expect.equal (Ok (BalancedBlock DisplayMath))
-
-         , test "image block" <|
-                  \_ ->
-                       "![yada]" |> run parse |>  Expect.equal (Ok (MarkdownBlock Image))
-
+            \_ ->
+                "```foo" |> run parse |> Expect.equal (Ok (BalancedBlock DisplayCode))
+        , test "verbatim block" <|
+            \_ ->
+                "````foo" |> run parse |> Expect.equal (Ok (BalancedBlock Verbatim))
+        , test "display math block" <|
+            \_ ->
+                "$$\n" |> run parse |> Expect.equal (Ok (BalancedBlock DisplayMath))
+        , test "image block" <|
+            \_ ->
+                "![yada]" |> run parse |> Expect.equal (Ok (MarkdownBlock Image))
         , test "unordered list item" <|
-                  \_ ->
-                       "- One" |> run parse |>  Expect.equal (Ok (MarkdownBlock UListItem))
-
+            \_ ->
+                "- One" |> run parse |> Expect.equal (Ok (MarkdownBlock UListItem))
         , test "ordered list item" <|
-                          \_ ->
-                               "2. foo" |> run parse |>  Expect.equal (Ok (MarkdownBlock OListItem))
-
+            \_ ->
+                "2. foo" |> run parse |> Expect.equal (Ok (MarkdownBlock (OListItem 0)))
         , test "quotation" <|
-                          \_ ->
-                               "> foo" |> run parse |>  Expect.equal (Ok (MarkdownBlock Quotation))
-
+            \_ ->
+                "> foo" |> run parse |> Expect.equal (Ok (MarkdownBlock Quotation))
         , test "poetry" <|
-                          \_ -> ">> Twas the night ..." |> run parse |>  Expect.equal (Ok (MarkdownBlock Poetry))
-
+            \_ -> ">> Twas the night ..." |> run parse |> Expect.equal (Ok (MarkdownBlock Poetry))
         , test "Heading level 1" <|
-                          \_ ->
-                               "# Introduction" |> run parse |>  Expect.equal (Ok (MarkdownBlock (Heading 1)))
-
-       , test "Heading level 2" <|
-                          \_ ->
-                               "## Insects" |> run parse |>  Expect.equal (Ok (MarkdownBlock (Heading 2)))
-
-      , test "Thematic break" <|
-                          \_ ->
-                               "___" |> run parse |>  Expect.equal (Ok (MarkdownBlock HorizontalRule))
-
-
+            \_ ->
+                "# Introduction" |> run parse |> Expect.equal (Ok (MarkdownBlock (Heading 1)))
+        , test "Heading level 2" <|
+            \_ ->
+                "## Insects" |> run parse |> Expect.equal (Ok (MarkdownBlock (Heading 2)))
+        , test "Thematic break" <|
+            \_ ->
+                "___" |> run parse |> Expect.equal (Ok (MarkdownBlock HorizontalRule))
         ]
