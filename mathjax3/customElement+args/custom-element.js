b89df699b118d513c391c8ed75ebe6da3a073d87
James Carlson <jxxcarlson@mac.com>
2019-09-10 23:14:19 -0500

    Args problem solved, copies to Tex folder

--- mathjax3/customElement+args/custom-element.js
+++ mathjax3/customElement+args/custom-element.js
@@ -3,23 +3,12 @@ class CustomElement extends HTMLElement {
     super();
     console.log("innerHTML: " + this.innerHTML)
     this.template = document.createElement('template');
-    this.template.innerHTML = `
-      <style>
-        :host {
-          color: tomato;
-	}
-      </style>
-      <p>MathML inside of a custom element: ${this.innerHTML}</p>
-    `;
-
-    console.log("template.content: " + JSON.stringify(this.template.content))
-
+    this.template.innerHTML = this.innerHTML;
     this.attachShadow({mode: "open"}).appendChild(this.template.content.cloneNode(true));
   }
 
   connectedCallback() {
     console.log("Calling back ...")
-    console.log("shadowRoot: " + JSON.stringify(this.shadowRoot))
     MathJax.typesetShadow(this.shadowRoot);
   }
 }
--- mathjax3/customElement-TeX/custom-element.js
+++ mathjax3/customElement-TeX/custom-element.js
@@ -1,25 +1,14 @@
 class CustomElement extends HTMLElement {
   constructor() {
     super();
-
+    console.log("innerHTML: " + this.innerHTML)
     this.template = document.createElement('template');
-    this.template.innerHTML = `
-      <style>
-        :host {
-          color: tomato;
-	}
-      </style>
-      <p>MathML inside of a custom element: <math><mfrac><mn>10</mn><mrow><mn>12</mn></mrow></mfrac></math></p>
-    `;
-
-    console.log("template.content: " + JSON.stringify(this.template.content))
-
+    this.template.innerHTML = this.innerHTML;
     this.attachShadow({mode: "open"}).appendChild(this.template.content.cloneNode(true));
   }
 
   connectedCallback() {
     console.log("Calling back ...")
-    console.log("shadowRoot: " + JSON.stringify(this.shadowRoot))
     MathJax.typesetShadow(this.shadowRoot);
   }
 }
