fee69a7d69110b1bc4d1ae76065258a98704ae0c
James Carlson <jxxcarlson@mac.com>
2019-09-10 23:46:11 -0500

    Remove unneeded import, remove console.log statements

--- mathjax3/customElement-MML/custom-element.js
+++ mathjax3/customElement-MML/custom-element.js
@@ -1,14 +1,12 @@
 class CustomElement extends HTMLElement {
   constructor() {
     super();
-    console.log("innerHTML: " + this.innerHTML)
     this.template = document.createElement('template');
     this.template.innerHTML = this.innerHTML;
     this.attachShadow({mode: "open"}).appendChild(this.template.content.cloneNode(true));
   }
 
   connectedCallback() {
-    console.log("Calling back ...")
     MathJax.typesetShadow(this.shadowRoot);
   }
 }
--- mathjax3/customElement/custom-element.js
+++ mathjax3/customElement/custom-element.js
@@ -12,14 +12,10 @@ class CustomElement extends HTMLElement {
       <p>MathML inside of a custom element: <math><mfrac><mn>10</mn><mrow><mn>12</mn></mrow></mfrac></math></p>
     `;
 
-    console.log("template.content: " + JSON.stringify(this.template.content))
-
     this.attachShadow({mode: "open"}).appendChild(this.template.content.cloneNode(true));
   }
 
   connectedCallback() {
-    console.log("Calling back ...")
-    console.log("shadowRoot: " + JSON.stringify(this.shadowRoot))
     MathJax.typesetShadow(this.shadowRoot);
   }
 }
