45f6805098dd51120ed6e43e2c298836b64aeac0
James Carlson <jxxcarlson@mac.com>
2019-09-14 15:25:15 -0500

    In colored-text, set things up in the connected call-back

--- mathjax3/colored-text/colored-text.js
+++ mathjax3/colored-text/colored-text.js
@@ -1,14 +1,18 @@
 
 class ColoredText extends HTMLElement {
-    constructor () {
-        super()
+    connectedCallback () {
+        const color = this.getAttribute('color')
+
         console.log("COLORED:innerHTML: " + this.innerHTML)
         const shadowDOM = this.attachShadow({ mode: 'open' })
+
         const text = document.createElement('p')
         text.innerHTML = this.innerHTML
         this.innerHTML = ''
+
         const style = document.createElement('style')
         style.innerHTML = `p { color: ${color} }`
+
         shadowDOM.appendChild(style)
         shadowDOM.appendChild(text)
     }
