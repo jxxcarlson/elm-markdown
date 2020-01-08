6d94a6e074c969f2eb1443e7ca0c7e0016c5641e
James Carlson <jxxcarlson@mac.com>
2020-01-08 15:31:58 +0100

    Add -d (for debug) option

--- app-demo-fancy/make.sh
+++ app-demo-fancy/make.sh
@@ -1,2 +1,9 @@
-elm make --optimize src/Main.elm --output=Main.js
-# elm make src/Main.elm --output=Main.js
+#!/bin/bash
+
+case $1 in
+-d)
+  elm make --debug src/Main.elm --output=Main.js
+  ;;
+*)
+  elm make --optimize src/Main.elm --output=Main.js
+esac
